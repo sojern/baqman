755efcd5c41bb8d79192e0c452a7160fe948a095
dp <ceocoder@users.noreply.github.com>
2020-10-02 15:19:03 -0700

    use go mod and update deps (#1)
    Co-authored-by: Dhaivat Pandit <dhaivatpandit@gmail.com>

---
+++ go.mod
@@ -0,0 +1,14 @@
+module baqman
+
+go 1.15
+
+require (
+	code.cloudfoundry.org/bytefmt v0.0.0-20200131002437-cf55d5288a48
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/sessions v1.2.1
+	github.com/onsi/ginkgo v1.14.1 // indirect
+	github.com/onsi/gomega v1.10.2 // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	google.golang.org/api v0.32.0
+)
--- main.go
+++ main.go
@@ -3,16 +3,24 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 )
 
 func main() {
 
-	projectID := flag.String("projectID", "sojern-bigquery", "List Queries running under project")
+	projectID := flag.String("projectID", "", "List Queries running under project")
 	port := flag.Int("port", 8080, "http port to listen on")
 	sessionSecret := flag.String("sessionSecret", "itsy bitsy spider climbed up the water spout", "Some nonsensical 'secure' string")
 
 	flag.Parse()
 
+	if *projectID == "" {
+		// try to get projectID from env
+		*projectID = os.Getenv("GOOGLE_CLOUD_PROJECT")
+		if *projectID == "" {
+			log.Fatalf("projectID can't be empty")
+		}
+	}
 	service := NewBQService(*projectID)
 	log.Printf("Running BaqMan for project: %v on port %d", *projectID, *port)
 	RunServer(service, *port, *sessionSecret)
